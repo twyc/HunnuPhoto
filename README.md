768193e7eddd72abff54a542d5c0c00263e95250
taifu <taifu@taifua.com>
2019-01-25 21:51:55 +0800

    Initial photo album frame.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HunnuPhoto
-A simple photo album.
+
+## Description
+
+It is just a simple photo album framework for now.
+
+## Demo
+
+![](https://ww1.sinaimg.cn/large/007i4MEmgy1fzj624fmvmj319c0mp43e.jpg)
---
+++ app/src/main/java/com/taifua/material/MainActivity.java
@@ -0,0 +1,142 @@
+package com.taifua.material;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener
+{
+
+    private DrawerLayout mDrawerLayout;
+    private ViewPager viewPager;
+    private NavigationView navView;
+    BottomNavigationView bottomNavigationView;
+    private PhotoFragment photoFragment = new PhotoFragment();
+    private AlbumFragment albumFragment = new AlbumFragment();
+    private AssistantFragment assistantFragment = new AssistantFragment();
+    private ShareFragment shareFragment = new ShareFragment();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.more:
+                Toast.makeText(this, "You clicked Setting", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.addOnPageChangeListener(this);
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+            @Override
+            public Fragment getItem(int i)
+            {
+                switch (i)
+                {
+                    case 0:
+                        return photoFragment;
+                    case 1:
+                        return albumFragment;
+                    case 2:
+                        return assistantFragment;
+                    case 3:
+                        return shareFragment;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount()
+            {
+                return 4;
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                viewPager.setCurrentItem(menuItem.getOrder());
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1)
+    {
+
+    }
+
+    @Override
+    public void onPageSelected(int i)
+    {
+        bottomNavigationView.getMenu().getItem(i).setChecked(true);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i)
+    {
+
+    }
+}
